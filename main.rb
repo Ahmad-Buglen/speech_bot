7520c64b41b957f3f4354f0b54b0360044621b4f
Zakhratulaev Ahmad <ahmad.buglen@gmail.com>
2022-12-15 23:37:00 +0300

    answer collecting update

--- main.rb
+++ main.rb
@@ -10,7 +10,7 @@ YC_ENDPOINT = 'https://storage.yandexcloud.net'
 YC_API = 'api.cloud.yandex.net'
 TG_BOT_API = 'https://api.telegram.org'
 WAIT_TIME = 2
-REPEAT_COUNT = 30
+REPEAT_COUNT = 15
 
 Aws.config.update(
   region: 'ru-central1',
@@ -64,6 +64,7 @@ def get_rezult(operation_id, file_name)
     response = HTTParty.get("https://operation.#{YC_API}/operations/#{operation_id}", option).to_h
     sleep WAIT_TIME
     break if response['done'] == true
+
     tries -= 1
   end
 
@@ -71,39 +72,28 @@ def get_rezult(operation_id, file_name)
                      bucket: ENV['YC_BUCKET_NAME'],
                      key: file_name
                    })
-  answer = nested_hash_value(response, 'text')
+
+  answer = response.dig('response', 'chunks')&.map { |e| e['alternatives']&.first&.dig('text') }&.join(' ')
   answer || 'Не смогли распознать :('
 end
 
 def telegram_bot
   Telegram::Bot::Client.run(ENV['TG_BOT_TOKEN']) do |bot|
-    begin
-      bot.listen do |message|
-        if message.instance_of?(Telegram::Bot::Types::Message) && message.voice 
-          file_name = voice_upload(message)
-          operation_id = speech_detect(file_name)
-          answer = get_rezult(operation_id, file_name)
-          bot.api.send_message(chat_id: message.chat.id, text: answer)
-        elsif message.instance_of?(Telegram::Bot::Types::Message) && message.text == '/start'
-          bot.api.send_message(chat_id: message.chat.id,
-                              text: "Привет, #{message.from.first_name}! Отправь голосовое, получи текст!")
-        else
-          bot.api.send_message(chat_id: message.chat.id, text: 'Распознаю только речь :)')
-        end
+    bot.listen do |message|
+      if message.instance_of?(Telegram::Bot::Types::Message) && message.voice
+        file_name = voice_upload(message)
+        operation_id = speech_detect(file_name)
+        answer = get_rezult(operation_id, file_name)
+        bot.api.send_message(chat_id: message.chat.id, text: answer)
+      elsif message.instance_of?(Telegram::Bot::Types::Message) && message.text == '/start'
+        bot.api.send_message(chat_id: message.chat.id,
+                             text: "Привет, #{message.from.first_name}! Отправь голосовое, получи текст!")
+      else
+        bot.api.send_message(chat_id: message.chat.id, text: 'Распознаю только речь :)')
       end
-    rescue Telegram::Bot::Exceptions::ResponseError => e
-      retry
     end
-  end
-end
-
-def nested_hash_value(obj, key)
-  if obj.respond_to?(:key?) && obj.key?(key)
-    obj[key]
-  elsif obj.respond_to?(:each)
-    r = nil
-    obj.find { |*a| r = nested_hash_value(a.last, key) }
-    r
+  rescue Telegram::Bot::Exceptions::ResponseError => e
+    retry
   end
 end
 
