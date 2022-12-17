843af46f14da201a38e6ce4e87ac758c592b43b0
Zakhratulaev Ahmad <ahmad.buglen@gmail.com>
2022-12-17 21:39:47 +0300

    code review

--- main.rb
+++ main.rb
@@ -2,94 +2,40 @@
 
 require 'sinatra'
 require 'telegram/bot'
-require 'aws-sdk-s3'
 require 'dotenv/load'
-require 'httparty'
+require_relative 'yandex_cloud_api'
 
-YC_ENDPOINT = 'https://storage.yandexcloud.net'
-YC_API = 'api.cloud.yandex.net'
-TG_BOT_API = 'https://api.telegram.org'
-WAIT_TIME = 2
-REPEAT_COUNT = 15
-
-Aws.config.update(
-  region: 'ru-central1',
-  credentials: Aws::Credentials.new(ENV['YC_ACCESS_KEY_ID'], ENV['YC_SECRET_ACCESS_KEY'])
-)
-
-def yc
-  yc ||= Aws::S3::Client.new(endpoint: YC_ENDPOINT)
-end
-
-def voice_upload(message)
-  path = HTTParty.get("#{TG_BOT_API}/bot#{ENV['TG_BOT_TOKEN']}/getFile?file_id=#{message.voice.file_id}").to_h.dig(
+def handle_voice(message, bot, yc_api)
+  path = bot.api.get_file(file_id: message.voice.file_id).dig(
     'result', 'file_path'
   )
-  file = HTTParty.get("#{TG_BOT_API}/file/bot#{ENV['TG_BOT_TOKEN']}/#{path}")&.body
+  file = HTTParty.get("https://api.telegram.org/file/bot#{ENV['TG_BOT_TOKEN']}/#{path}")&.body
+
   name = "#{message.chat.id}_#{message.message_id}.ogg'"
-  yc.put_object({
-                  bucket: ENV['YC_BUCKET_NAME'],
-                  key: name,
-                  body: file
-                })
-
-  name
+  yc_api.file_upload(file, name)
+  operation_id = yc_api.speech_detect(name)
+  text = yc_api.get_rezult(operation_id)
+  bot.api.send_message(chat_id: message.chat.id, text: text)
+  yc_api.file_remove(name)
 end
 
-def speech_detect(file_name)
-  options = {
-    headers: { 'Authorization' => "Api-Key #{ENV['YC_API_KEY']}" },
-    body: {
-      'config' => {
-        'specification' => {
-          'languageCode' => 'ru-RU'
-        }
-      },
-      'audio' => {
-        'uri' => "#{YC_ENDPOINT}/#{ENV['YC_BUCKET_NAME']}/#{file_name}"
-      }
-    }.to_json
-  }
-  HTTParty.post("https://transcribe.#{YC_API}/speech/stt/v2/longRunningRecognize",
-                options).to_h['id']
-end
-
-def get_rezult(operation_id, file_name)
-  option = {
-    headers: { 'Authorization' => "Api-Key #{ENV['YC_API_KEY']}" }
-  }
-
-  tries = REPEAT_COUNT
-  while tries.positive?
-    response = HTTParty.get("https://operation.#{YC_API}/operations/#{operation_id}", option).to_h
-    sleep WAIT_TIME
-    break if response['done'] == true
-
-    tries -= 1
+def handle_other(message, bot)
+  if message.instance_of?(Telegram::Bot::Types::Message) && message.text == '/start'
+    bot.api.send_message(chat_id: message.chat.id,
+                         text: "Привет, #{message.from.first_name}! Отправь голосовое, получи текст!")
+  else
+    bot.api.send_message(chat_id: message.chat.id, text: 'Распознаю только речь :)')
   end
-
-  yc.delete_object({
-                     bucket: ENV['YC_BUCKET_NAME'],
-                     key: file_name
-                   })
-
-  answer = response.dig('response', 'chunks')&.map { |e| e['alternatives']&.first&.dig('text') }&.join(' ')
-  answer || 'Не смогли распознать :('
 end
 
 def telegram_bot
+  yc_api = YandexCloudAPI.new
   Telegram::Bot::Client.run(ENV['TG_BOT_TOKEN']) do |bot|
     bot.listen do |message|
       if message.instance_of?(Telegram::Bot::Types::Message) && message.voice
-        file_name = voice_upload(message)
-        operation_id = speech_detect(file_name)
-        answer = get_rezult(operation_id, file_name)
-        bot.api.send_message(chat_id: message.chat.id, text: answer)
-      elsif message.instance_of?(Telegram::Bot::Types::Message) && message.text == '/start'
-        bot.api.send_message(chat_id: message.chat.id,
-                             text: "Привет, #{message.from.first_name}! Отправь голосовое, получи текст!")
+        handle_voice(message, bot, yc_api)
       else
-        bot.api.send_message(chat_id: message.chat.id, text: 'Распознаю только речь :)')
+        handle_other(message, bot)
       end
     end
   rescue Telegram::Bot::Exceptions::ResponseError => e
--- yandex_cloud_api.rb
+++ yandex_cloud_api.rb
@@ -1,24 +1,45 @@
 # frozen_string_literal: true
 
+require 'aws-sdk-s3'
+require 'httparty'
+
+YC_API = 'api.cloud.yandex.net'
+YC_ENDPOINT = 'https://storage.yandexcloud.net'
+WAIT_TIME = 2
+REPEAT_COUNT = 15
+
+Aws.config.update(
+  region: 'ru-central1',
+  credentials: Aws::Credentials.new(ENV['YC_ACCESS_KEY_ID'], ENV['YC_SECRET_ACCESS_KEY'])
+)
+
 class YandexCloudAPI
+  attr_reader :client, :endpoint, :bucket, :header_api_key
+
+  def initialize(bucket = ENV['YC_BUCKET_NAME'])
+    @client = Aws::S3::Client.new(endpoint: YC_ENDPOINT)
+    @bucket = bucket
+    @header_api_key = { 'Authorization' => "Api-Key #{ENV['YC_API_KEY']}" }
+  end
+
   def file_upload(file, name)
-    Aws::S3::Client.new(endpoint: YC_ENDPOINT).put_object({
-                                                            bucket: ENV['YC_BUCKET_NAME'],
-                                                            key: name,
-                                                            body: file
-                                                          })
+    client.put_object({
+                        bucket: bucket,
+                        key: name,
+                        body: file
+                      })
   end
 
   def file_remove(name)
-    Aws::S3::Client.new(endpoint: YC_ENDPOINT).delete_object({
-                                                               bucket: ENV['YC_BUCKET_NAME'],
-                                                               key: name
-                                                             })
+    client.delete_object({
+                           bucket: bucket,
+                           key: name
+                         })
   end
 
   def speech_detect(file_name)
     options = {
-      headers: { 'Authorization' => "Api-Key #{ENV['YC_API_KEY']}" },
+      headers: header_api_key,
       body: {
         'config' => {
           'specification' => {
@@ -26,11 +47,25 @@ class YandexCloudAPI
           }
         },
         'audio' => {
-          'uri' => "#{YC_ENDPOINT}/#{ENV['YC_BUCKET_NAME']}/#{file_name}"
+          'uri' => "#{YC_ENDPOINT}/#{bucket}/#{file_name}"
         }
       }.to_json
     }
     HTTParty.post("https://transcribe.#{YC_API}/speech/stt/v2/longRunningRecognize",
                   options).to_h['id']
+  end
+
+  def get_rezult(operation_id)
+    tries = REPEAT_COUNT
+    while tries.positive?
+      response = HTTParty.get("https://operation.#{YC_API}/operations/#{operation_id}",
+                              { headers: header_api_key }).to_h
+      sleep WAIT_TIME
+      break if response['done'] == true
+
+      tries -= 1
+    end
+    answer = response.dig('response', 'chunks')&.map { |e| e['alternatives']&.first&.dig('text') }&.join(' ')
+    answer || 'Не смогли распознать :('
   end
 end
